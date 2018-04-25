8ceb9bfce82444a21a77fb5fe1e726bf1cb567c4
Mike C <mikecronin92@gmail.com>
2018-04-25 00:36:35 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,3 @@
-binding.pry
-
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -8,9 +6,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
-    binding.pry
     BRANDS.uniq
-    
   end
 
   def cobble
